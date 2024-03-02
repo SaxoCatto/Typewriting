aedf9ddfaa059e0d0967dfb4d8310b3ab0ab5bf1
Saxo Cat <92432250+SaxoCatto@users.noreply.github.com>
2024-03-02 21:15:49 +0700

    Create interactive-shell.sh

---
+++ interactive-shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# can use whoami
+echo "Tell me your name."
+read name
+echo "How old are you?"
+read age
+echo "Wow, $name, didn't know you were $age years old"
+
+# I want to have the input string checked
